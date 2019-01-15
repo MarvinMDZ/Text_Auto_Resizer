3bc376b4ba78f33e674513603d9b66c65d027b03
Javier Egido <javier.egido.alonso@gmail.com>
2019-01-15 13:40:02 +0100

    fix first iteration issue

--- example/script.js
+++ example/script.js
@@ -3,17 +3,17 @@ function resizeMe(className){
 	
 	var resElement;
 	var prefix = className.substring(0,1) !== '.' ? '.' : '';
+	var item;
+	var currentFontSize;
 
 	resElement = document.querySelectorAll(prefix + className);	
 	
 	for (var i = resElement.length - 1; i >= 0; i--) {
-		var item;
-		var currentFontSize;
-		do{
-			item = resElement[i];
+		item = resElement[i];
+		while(item.parentElement.clientWidth < item.clientWidth || item.parentElement.clientHeight < item.clientHeight){
 			currentFontSize = parseFloat(window.getComputedStyle(item, null).getPropertyValue('font-size'));
 			item.style.fontSize = (currentFontSize - 1)+'px';
-		}while(item.parentElement.clientWidth < item.clientWidth || item.parentElement.clientHeight < item.clientHeight);
+		}
 	}
 
 }
